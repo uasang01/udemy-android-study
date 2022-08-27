2f40664db88f97d4c8227384e08042f0eb532dde
SanghoonKim <uasang01@nate.com>
2022-08-27 19:32:39 +0900

    set nav host fragment

---
+++ Navigation Architecture Component/NavDemo1/app/src/main/java/com/anushka/navdemo1/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.anushka.navdemo1.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
+    }
+}
